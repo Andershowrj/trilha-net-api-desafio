3e92cbc84357f510f4cce909273451b7e18a873c
Anderson Martins <andersonmcadastros@gmail.com>
2023-01-29 19:05:57 +0000

    Projeto finalizado

--- Context/OrganizadorContext.cs
+++ Context/OrganizadorContext.cs
@@ -3,13 +3,15 @@ using TrilhaApiDesafio.Models;
 
 namespace TrilhaApiDesafio.Context
 {
+    //DbContext é a classe pai que vem do EntityFramework
     public class OrganizadorContext : DbContext
     {
+        //Recebe a configuração do Db
         public OrganizadorContext(DbContextOptions<OrganizadorContext> options) : base(options)
         {
             
         }
-
+        //Propriedade que se refera a entidade (uma classe no programa e uma tabela no Db)
         public DbSet<Tarefa> Tarefas { get; set; }
     }
 }
